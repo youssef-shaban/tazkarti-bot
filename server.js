e7b3cc75fdff46eda1909cfe5ee72e42ea4e46c3
youssef Shaban <85808789+youssef-shaban@users.noreply.github.com>
2023-04-07 22:30:06 +0200

    handle when tazkarti is down

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const corn = require("node-cron");
-const request = require("request");
+const axios = require("axios");
 const dotenv = require("dotenv");
 const telegram = require("./src/telegramBot.js");
 const messageHandler = require("./src/messageHandler.js");
@@ -9,43 +9,56 @@ dotenv.config({ path: "./config.env" });
 const app = express();
 app.use(express.json());
 
-let options = {
-  method: "GET",
-  url: process.env.MATCH_REQUEST,
+let oldRes = [];
+let getMatchesDone = false;
+
+const getMatches = async () => {
+  while (true) {
+    const response = await axios.get(process.env.MATCH_REQUEST, {
+      validateStatus: (status) => {
+        return true;
+      },
+    });
+    if (response.status == 200) {
+      oldRes = response.data;
+      getMatchesDone = true;
+      break;
+    }
+    console.log("💥💥 WRONG REQUEST");
+  }
 };
 
-let oldRes = [];
-
-request(options, (err, res) => {
-  if (err) throw new Error(err);
-
-  oldRes = JSON.parse(res.body);
-});
-
-corn.schedule("*/2 * * * * *", () => {
-  request(options, (err, res) => {
-    if (err) throw new Error(err);
-    let newRes = JSON.parse(res.body);
-    let addedMatches = newRes.filter(
-      (item) => !oldRes.some((other) => item.matchId === other.matchId)
-    );
-
-    if (addedMatches.length > 0) {
-      addedMatches.forEach((element) => {
-        telegram.SendMessage(messageHandler.matchMessage(element));
-        // console.log(messageHandler.matchMessage(element));
-      });
-    }
-    console.log(`there is ${addedMatches.length} added matches.`);
-    oldRes = newRes;
-  });
-});
-
+getMatches();
 app.use("/api/v1/", (req, res, next) => {
   res.status(200).json({
     status: "success",
     massage: "Welcome to tazkarti bot",
   });
+});
+
+corn.schedule("*/2 * * * * *", async () => {
+  if (!getMatchesDone) return;
+  let res = await axios.get(process.env.MATCH_REQUEST, {
+    validateStatus: function (status) {
+      return true;
+    },
+  });
+  if (res.status != 200) {
+    console.log("💥💥 WRONG REQUEST");
+    return;
+  }
+  const newRes = res.data;
+  const addedMatches = newRes.filter(
+    (item) => !oldRes.some((other) => item.matchId === other.matchId)
+  );
+
+  addedMatches.forEach((element) => {
+    telegram.SendMessage(messageHandler.matchMessage(element));
+    // console.log(messageHandler.matchMessage(element));
+  });
+
+  console.log(`there is ${addedMatches.length} added matches.`);
+  oldRes = newRes;
 });
 
 const port = 8080;
